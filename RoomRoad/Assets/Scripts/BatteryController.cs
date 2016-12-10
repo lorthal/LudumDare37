a82694bbad48795b913fa7b361fc5d694cd339fa
Flurrih <ghost744@gmail.com>
2016-12-10 10:48:56 +0100

    Zajebisty commit

---
+++ RoomRoad/Assets/Scripts/BatteryController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatteryController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            Recharge(collider.GetComponent<PlayerController>());
+        }
+    }
+
+    void Recharge(PlayerController player)
+    {
+        Debug.Log("Player is being recharged...");
+    }
+}
---
+++ RoomRoad/Assets/Scripts/PickupController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            PickUp(collider.GetComponent<PlayerController>());
+        }
+    }
+
+    void PickUp(PlayerController player)
+    {
+        Debug.Log("Picked up!");
+    }
+}
