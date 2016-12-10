9e1399558d4e1fa5383f4b9501e62afe7f56f2dd
Flurrih <ghost744@gmail.com>
2016-12-10 13:08:00 +0100

    Poprawione kontrolery

---
+++ RoomRoad/Assets/EnergyController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyController : MonoBehaviour {
+
+    float energyMaxAmount = 100.0f;
+    float energyCurrentAmount;
+
+	// Use this for initialization
+	void Start () {
+        energyCurrentAmount = energyMaxAmount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        energyCurrentAmount -= Time.deltaTime;
+        if (energyCurrentAmount <= 0)
+            Debug.Log("Rozladowany XD");
+
+        Debug.Log(energyCurrentAmount);
+	}
+
+    public void RechargeBattery()
+    {
+        energyCurrentAmount = energyMaxAmount;
+    }
+
+    public float GetEnergyAmount()
+    {
+        return energyCurrentAmount;
+    }
+}
---
+++ RoomRoad/Assets/Scripts/PickupRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupRotation : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+    }
+}
