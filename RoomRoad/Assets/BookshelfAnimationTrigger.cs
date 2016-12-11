7039eb09229ab49458ab4867471715adafe2c9ce
Flurrih <ghost744@gmail.com>
2016-12-11 18:25:16 +0100

    Animacja shelfa

---
+++ RoomRoad/Assets/BookshelfAnimationTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BookshelfAnimationTrigger : MonoBehaviour {
+
+    enum ShelfState { Activated, Deactivated }
+
+    public GameObject obj;
+
+    void Start()
+    {
+        obj.GetComponent<Animator>().Stop();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        Debug.Log("S");
+        if(coll.gameObject.tag == "Player")
+        {
+            obj.GetComponent<Animation>().Play("Anim1");
+        }
+    }
+}
