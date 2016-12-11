fbe93f4b301bcc7d3b32dba15b5d29e3c707556b
Lorthal <bkluchcinski@gmail.com>
2016-12-11 21:49:39 +0100

    update dźwięku i animacja lampy

---
+++ RoomRoad/Assets/LampAnimTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampAnimTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+        {
+            GetComponentInParent<Animator>().SetTrigger("StartAnim");
+        }
+    }
+}
