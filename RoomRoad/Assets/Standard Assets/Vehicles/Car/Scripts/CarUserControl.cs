28599c6b61557bb0641f6d814ad3822ae340648d
Lorthal <bkluchcinski@gmail.com>
2016-12-11 18:41:04 +0100

    efekt dla dywanu

--- RoomRoad/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ RoomRoad/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
-    [RequireComponent(typeof (CarController))]
+    [RequireComponent(typeof(CarController))]
     public class CarUserControl : MonoBehaviour
     {
         private CarController m_Car; // the car controller we want to use
+
+        public float multiplier { get; set; }
 
 
         private void Awake()
@@ -15,12 +17,16 @@ namespace UnityStandardAssets.Vehicles.Car
             m_Car = GetComponent<CarController>();
         }
 
+        private void Start()
+        {
+            multiplier = 1;
+        }
 
         private void FixedUpdate()
         {
             // pass the input to the car!
             float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
+            float v = Input.GetAxis("Vertical") * multiplier;
             m_Car.Move(h, v, v, 0);
         }
     }
