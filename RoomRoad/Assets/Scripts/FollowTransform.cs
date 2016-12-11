51219a11daea3859701be182fc242d8a45120bc1
Lorthal <bkluchcinski@gmail.com>
2016-12-11 10:28:19 +0100

    podmieniony model

--- RoomRoad/Assets/Scripts/FollowTransform.cs
+++ RoomRoad/Assets/Scripts/FollowTransform.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class FollowTransform : MonoBehaviour {
     [SerializeField]
     Transform target;
+    [SerializeField]
+    float rotationSpeed;
     Vector3 offset;
 
     void Awake()
@@ -13,5 +15,9 @@ public class FollowTransform : MonoBehaviour {
 
 	void Update () {
         transform.position = target.position + offset;
+
+
+        //transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, new Vector3(0, Mathf.Abs(target.rotation.eulerAngles.y),0), Time.deltaTime * rotationSpeed)));
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime * rotationSpeed);
 	}
 }
