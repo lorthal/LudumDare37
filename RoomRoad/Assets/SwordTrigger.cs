70e285cc4f3ebdbc0b426af8adbc813db653e955
Flurrih <ghost744@gmail.com>
2016-12-12 10:47:51 +0100

    Animacja miecza

---
+++ RoomRoad/Assets/SwordTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordTrigger : MonoBehaviour {
+    
+
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            GetComponentInParent<Animator>().SetTrigger("SwordTrigger");
+        }
+    }
+}
