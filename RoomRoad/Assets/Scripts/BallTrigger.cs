f5553f1afeeba97bde1241d9c84c0145bf078018
Lorthal <bkluchcinski@gmail.com>
2016-12-12 21:52:03 +0100

    popraweczka dla piłeczki

--- RoomRoad/Assets/Scripts/BallTrigger.cs
+++ RoomRoad/Assets/Scripts/BallTrigger.cs
@@ -12,7 +12,7 @@ public class BallTrigger : MonoBehaviour {
         {
             Debug.Log("Ball");
             collider.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            collider.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-0.25f, 1, 0.125f).normalized * force);
+            collider.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-0.3f, 1, 0.125f).normalized * force);
         }
     }
 }
