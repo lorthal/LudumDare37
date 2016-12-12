3b1a21f49412b0973af724dd04dd2b6e8911c8ae
Lorthal <bkluchcinski@gmail.com>
2016-12-12 10:59:52 +0100

    animacja szafy

---
+++ RoomRoad/Assets/SzafaAnimTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SzafaAnimTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            GetComponentInParent<Animator>().SetTrigger("OpenTrigger");
+        }
+    }
+}
