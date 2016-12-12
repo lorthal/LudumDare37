06bde135136aaff7bd11f18754c1f686545e56fc
Flurrih <ghost744@gmail.com>
2016-12-12 19:00:42 +0100

    added game over trigger to bin

---
+++ RoomRoad/Assets/GameOverTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+}
