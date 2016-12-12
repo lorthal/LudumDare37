395245bf019b8ccaaab3537d3403b1d1f59d0ae2
Flurrih <ghost744@gmail.com>
2016-12-12 10:11:50 +0100

    Bookshelf fix

--- RoomRoad/Assets/BookshelfAnimationTrigger.cs
+++ RoomRoad/Assets/BookshelfAnimationTrigger.cs
@@ -4,20 +4,23 @@ using System.Collections;
 public class BookshelfAnimationTrigger : MonoBehaviour {
 
     enum ShelfState { Activated, Deactivated }
+    ShelfState state;
 
     public GameObject obj;
 
     void Start()
     {
+        state = ShelfState.Deactivated;
         obj.GetComponent<Animator>().Stop();
     }
 
     void OnTriggerEnter(Collider coll)
     {
         Debug.Log("S");
-        if(coll.gameObject.tag == "Player")
+        if(coll.gameObject.tag == "Player" && state == ShelfState.Deactivated)
         {
             obj.GetComponent<Animation>().Play("Anim1");
+            state = ShelfState.Activated;
         }
     }
 }
